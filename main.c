1a808a12d110de83bc18947e544a531d6c813dc6
yohanankling <93263233+yohanankling@users.noreply.github.com>
2021-11-04 17:28:45 +0200

    Update main.c

--- main.c
+++ main.c
@@ -11,48 +11,37 @@ temp=max;
 max=min;
 min=temp;}
 int ans=0;
-printf("The Armstrong numbers are: ");
-for (int i=min;i<max;i++){
+printf("The Armstrong numbers are:");
+for (int i=min;i<=max;i++){
 ans = isArmstrong(i);
   if  (ans==1){
-    printf("%d ",i);}
+    printf(" %d",i);}
 }
-ans = isArmstrong(max);
-if (ans==1)
-    printf("%d",max);
 printf("\n");
 
-printf("The Palindrome are: ");
-for (int i=min;i<max;i++){
+printf("The Palindromes are:");
+for (int i=min;i<=max;i++){
 ans = isPalindrome(i);
   if  (ans==1){
-    printf("%d ",i);}
+    printf(" %d",i);}
 }
-ans= isPalindrome(max);
-if (ans==1)
-    printf("%d",max);
 printf("\n");
 
-printf("The Prime numbers are: ");
-for (int i=min;i<max;i++){
+printf("The Prime numbers are:");
+for (int i=min;i<=max;i++){
 ans = isPrime(i);
   if  (ans==1){
-    printf("%d ",i);}
+    printf(" %d",i);}
 }
-ans = isPrime(max);
-if (ans==1)
-    printf("%d",max);
 printf("\n");
 
-printf("The Strong numbers are: ");
-for (int i=min;i<max;i++){
+printf("The Strong numbers are:");
+for (int i=min;i<=max;i++){
 ans = isStrong(i);
   if  (ans==1){
-    printf("%d ",i);}
+    printf(" %d",i);}
 }
-ans = isStrong(max);
-if (ans==1){
-    printf("%d",max);}
+printf("\n");
 
     return 0;
 }
