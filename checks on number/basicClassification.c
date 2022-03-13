44e8eeaeaa7d7fa233eb86674a8c498fb26c4264
yohanankling <93263233+yohanankling@users.noreply.github.com>
2022-03-13 11:23:47 +0200

    Add files via upload

---
+++ checks on number/basicClassification.c
@@ -0,0 +1,42 @@
+int factorial(int actualFactorial);
+
+   int isPrime(int n) 
+   {
+        int ans = 1;
+        for (int i = 2; i <= n/2; i++) {
+            if (n == 1) {
+                ans = 1;
+            }
+            if (n!=i && n % i == 0) {
+                return 0;
+            }
+        }
+        return ans;
+    }
+
+int factorial(int actualFactorial) 
+ {
+        int thisSum=1;
+        for(int i=1;i<=actualFactorial;i++)
+        {
+            thisSum=thisSum*i;
+        }
+        return thisSum;
+    }
+
+int isStrong(int n) { 
+ int sum=0;
+        int temp=n;
+        int thisnum=0;
+        while(temp!=0)
+        {
+            thisnum=temp%10;
+            int finallFactorial=factorial(thisnum);
+            temp=temp/10;
+            sum=sum+finallFactorial;
+        }
+        if(sum==n)
+            return 1;
+        else
+            return 0;
+    }
